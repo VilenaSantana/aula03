1e531eb56c8fe2001ea982598116c890e3a02fd0
unknown <vilenalsantana@gmail.com>
2025-04-01 14:01:19 -0300

    Primeiro exercício

---
+++ ex01.py
@@ -0,0 +1,4 @@
+name = input("Qual o seu nome? ")
+id = input("Qual sua idade? ")
+money = input("Qual o seu salário? ")
+print(f"Oi {name}, você tem {id} anos e recebe por mês um salário no valor de R${money}.")
