545c5d396c183c6f7d5197f8f8770eb1c4a6271d
unknown <vilenalsantana@gmail.com>
2025-04-01 15:47:55 -0300

    Quinto exercício

---
+++ ex05.py
@@ -0,0 +1,11 @@
+print("                                     ")
+print("                                     ")
+n1 = float(input("Digite o primeiro número: "))
+print("                                     ")
+n2 = float(input("Digite o segundo número: "))
+print("                                     ")
+print("                                     ")
+if n1 > n2 :
+    print(n2,n1)
+else:
+    print(n1,n2)
